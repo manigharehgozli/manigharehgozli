435e7f5d3719bf42e6b9404976bff579cd943b28
mani <mmaanniigghh@gmail.com>
2026-02-16 17:57:48 -0800

    finish project

--- src/script.js
+++ src/script.js
@@ -1,16 +1,47 @@
+const titles = document.querySelectorAll(".section-title");
+const descriptions = document.querySelectorAll(".section-description");
+const icons = document.querySelectorAll(".button-icon");
+
+
 function changeImageBasedOnScreen() {
-  const imageElement = document.querySelector('.image1');
-  const screenWidth = window.innerWidth;
-  
-  if (screenWidth <= 768) {
-    imageElement.src = '/images/background-pattern-mobile.svg';
-    imageElement.alt = 'mobile img';
+  const imageElement = document.querySelector(`.background-image`);
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth <= 768) {
+    imageElement.src = `/images/background-pattern-mobile.svg`;
+    imageElement.alt = "mobile img";
   } else {
-    imageElement.src = '/images/background-pattern-desktop.svg';
-    imageElement.alt ='desktop img';
+    imageElement.src = `/images/background-pattern-desktop.svg`;
+    imageElement.alt = "desktop img";
   }
 }
 
-window.addEventListener('load', changeImageBasedOnScreen);
+window.addEventListener(`load`, changeImageBasedOnScreen);
+window.addEventListener(`resize`, changeImageBasedOnScreen);
 
-window.addEventListener('resize', changeImageBasedOnScreen);
+
+
+titles.forEach((title, i) => {
+  title.addEventListener("click", () => toggleItem(i));
+});
+
+icons.forEach((icon, i) => {
+  icon.addEventListener("click", () => toggleItem(i));
+});
+
+function toggleItem(index) {
+  descriptions.forEach((desc, i) => {
+    if (i === index) {
+      const isHidden = desc.classList.contains("hidden");
+
+      desc.classList.toggle("hidden", !isHidden);
+
+      icons[i].src = isHidden
+        ? "/images/icon-minus.svg"
+        : "/images/icon-plus.svg";
+    } else {
+      desc.classList.add("hidden");
+      icons[i].src = "/images/icon-plus.svg";
+    }
+  });
+}
